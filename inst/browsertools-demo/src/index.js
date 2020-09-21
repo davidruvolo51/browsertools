7ef6e324daf27f41fb7717e71421182bea3a67b5
davidruvolo51 <davidruvolo51@gmail.com>
2020-09-21 19:07:45 +0200

    rebuilt static assets

--- inst/browsertools-demo/src/index.js
+++ inst/browsertools-demo/src/index.js
@@ -2,12 +2,19 @@
 // FILE: index.js
 // AUTHOR: David Ruvolo
 // CREATED: 2020-05-25
-// MODIFIED: 2020-05-25
+// MODIFIED: 2020-09-21
 // PURPOSE: for building static assets
 // DEPENDENCIES: see package.json
-// STATUS: in.progress
+// STATUS: working
 // COMMENTS: NA
 ////////////////////////////////////////////////////////////////////////////////
 // BEGIN
 
-import "./index.scss"
+import "./scss/index.scss";
+
+// set document meta attributes that aren't available through Shiny
+window.addEventListener("DOMContentLoaded", (e) => {
+    const html = document.getElementsByTagName("html")[0];
+    html.lang = "en";
+    html.dir = "ltr";
+}, { once: true });
